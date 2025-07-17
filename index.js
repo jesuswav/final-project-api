5b30dc3a916fc4da22329e40b733b44676c8f68c
Jesus García <garciajesse2003@gmail.com>
2025-07-17 12:17:04 -0600

    [add] cors update

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { getTemperature, getHumedad, getHumedadTierra, getMediciones } = require('./src/functions')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const {
+  getTemperature,
+  getHumedad,
+  getHumedadTierra,
+  getMediciones,
+} = require("./src/functions");
+
+// Permitir solicitudes desde cualquier origen
+app.use(cors());
+
+// También necesitas esto si vas a recibir JSON
+app.use(express.json());
 
 const app = express();
 const PORT = 3008;
 
 // Midelwares
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // routes
-app.use('/api/mediciones', getMediciones)
-app.use('/api/temperatura', getTemperature)
-app.use('/api/humedad', getHumedad)
-app.use('/api/humedad_tierra', getHumedadTierra)
+app.use("/api/mediciones", getMediciones);
+app.use("/api/temperatura", getTemperature);
+app.use("/api/humedad", getHumedad);
+app.use("/api/humedad_tierra", getHumedadTierra);
 
 // endpoint por defecto
 app.get("/", (req, res) => {
-    res.send("API para registrar datos de mediciones con MySQL");
+  res.send("API para registrar datos de mediciones con MySQL");
 });
 
 // Iniciar servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
